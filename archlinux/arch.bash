49c5fff3cdf6eb8eecc0ade99d5e65bd0efd3531
Diego Vila <dego_vila@yahoo.com>
2022-11-27 21:17:57 -0400

    got it working on fedora

--- archlinux/arch.bash
+++ archlinux/arch.bash
@@ -1,15 +1,24 @@
 
 arch(){
-  if [ "$1" = "run" ]; then
-    podman run \
+  if [ -z "$2" ]; then
+    tag="1.0.0"
+  else
+    tag=$2
+  fi
+
+  SOURCE_DIR=$(dirname $BASH_SOURCE)
+ 
+  if [ "$1" = "build" ]; then
+    command='buildah bud -t arch:$tag $SOURCE_DIR'
+  elif [ "$1" = "run" ]; then
+    command='podman run \
       --interactive \
       --tty \
       --name arch \
       --volume $PWD:/home/ruahman/code \
       --workdir /home/ruahman/code \
       --entrypoint /bin/zsh \
-      arch:1.0.0 \
-      "$@"
+      arch:1.0.0'
   elif [ "$1" = "start" ]; then
     echo "...arch start"
     podman container start arch
@@ -21,16 +30,7 @@ arch(){
   elif [ "$1" = "rm" ]; then
     podman container rm arch
   else
-    command='podman run \
-      --interactive \
-      --tty \
-      --rm \
-      --name arch \
-      --volume $PWD:/home/ruahman/code \
-      --workdir /home/ruahman/code \
-      --entrypoint /bin/zsh \
-      arch:1.0.0 \
-      "$@"'
+    command='podman run -it --rm -v $PWD:/code --workdir /code arch:$tag /bin/zsh'
   fi
 
   echo $command
